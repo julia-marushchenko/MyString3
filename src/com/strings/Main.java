00186d8512bd6e80921f0c982f1c14f69924f295
Julia Marushchenko <marushchenko.yulia@gmail.com>
2026-01-11 03:28:38 +0100

    Add files via upload
    Committing Main class.

---
+++ src/com/strings/Main.java
@@ -0,0 +1,22 @@
+/**
+ *  Java program to use charAt() method for String.
+ */
+
+package com.strings;
+
+/**
+ *  Main class.
+ */
+public class Main {
+
+    // Main class to run java program.
+    public static void main(String[] args) {
+
+        // Creating string.
+        String letters = "ABCDEFGHIJKLMN";
+
+        // Finding index of symbol 'GHI'
+        System.out.println(letters.indexOf("GHI")); // Output: 6, because of index of first character 'G'.
+
+    }
+}
